48481731768837ff208b07b9dd541f32ddbd8557
Aditi Raghunandan <117427481+Champakaly@users.noreply.github.com>
2023-01-30 07:02:38 +0530

    Create movepages.js

---
+++ movepages.js
@@ -0,0 +1,10 @@
+function scrollright() {
+    var element = document.getElementById("scrollmenu");
+    element.scrollLeft += 20;
+    }
+   
+    function scrollleft(){
+        var element = document.getElementById("scrollmenu");
+    element.scrollLeft += -20;
+    }
+    
